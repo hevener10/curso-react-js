90500e6b6c2e3f1ac4049a50d915200365cf5b06
Hevener Ancelmo Pereira <hevener2@hotmail.com>
2023-05-31 07:31:25 -0300

    Primeiro Commit

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+function Banner() {
+    return (
+        <header>
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+        </header>
+    )
+
+}
+
+export default Banner
